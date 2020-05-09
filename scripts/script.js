169f38f66c8baebb80b04a8ebf1df0f8e73aa8a2
lena-vllz <lenavulliez@gmail.com>
2020-05-09 14:20:44 +0200

    change words

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,9 @@ const displayText = document.querySelector('.words')
 const less = document.querySelector('.key29')
 const more = document.querySelector('.key30')
 let themeContent = 'song 1'
-
+const change = document.querySelectorAll('.sound')
+const words1 =['♫♫','wrok it', 'make it','do it','make us','♫♫','harder','better','faster','stronger','song 2','♫♫','more than', 'hour', 'our','never', '♫♫','ever','after','work is','over','♫♫','work it','make it','do it', 'make us', '♫♫']
+const words2 =['♫♫','♫♫','pain','made me','made me','believer','believer','♫♫','pain','break me','song 1','build me','believer','believer','pain','bullets fly','rain','my life', 'my love','my drive','came from','♫♫','pain','made me','made me','believer',"believer"]
 displayText.textContent = 'Welcome';
 document.documentElement.setAttribute('data-theme', 'default');
 
@@ -57,85 +59,36 @@ window.addEventListener('keydown', audioPlay)
 // Function that display different theme colors and song
 function changeTheme(e) {
     if (e.code == "Enter" && themeContent == 'song 2') {
-        themeContent = 'song 2'
+        themeContent = 'song 1'
         document.documentElement.setAttribute('data-theme', 'default');
         themeContent.textContent = 'song 2'
         // Remove the seconde audio on keydown
         window.removeEventListener('keydown', audioPlay2)
         // Put the new audio  on keydown
         window.addEventListener('keydown', audioPlay)
+        for (i = 0 ; i < words1.length; i++) {
+            change[i].textContent = words1[i]
+        }
         
     } else if (e.code == "Enter" && themeContent == 'song 1') {
-        themeContent = 'song 1'
+        themeContent = 'song 2'
         document.documentElement.setAttribute('data-theme', 'song2');
         themeContent.textContent = 'song 1'
         window.removeEventListener('keydown', audioPlay)
         window.addEventListener('keydown', audioPlay2)
+        for (i = 0 ; i < words2.length; i++) {
+            change[i].textContent = words2[i]
+        }
     }
 }
 
 // ON CLICKKKKKK
 
-// function clickAudio(event) 
-// {
-//     const padsName = event.srcElement.className;
-//     const padsClass = document.querySelector(`.key[class='${padsName}']`)
-//     const padsKey = padsClass.getAttribute('data-key')
-//     const pads = document.querySelector(`.box[data-key='${padsKey}']`)
-//     pads.classList.add('playing');
-//     const audio = document.querySelector(`audio[data-key='${padsKey}']`)
-//     if(!audio) return;
-    
-//     const nameAudio = audio.getAttribute('data-id')
-//     textDisplay.textContent = nameAudio
-
-//     audio.currentTime = 0
-//     audio.play()
-// }
-
-
-// function clickAudio2(event) 
-// {
-//     const padsName = event.srcElement.className;
-//     const padsClass = document.querySelector(`.key[class='${padsName}']`)
-//     const padsKey = padsClass.getAttribute('data-key')
-
-//     const pads = document.querySelector(`.box[data-key='${padsKey}']`)
-//     pads.classList.add('playing');
-//     const audio = document.querySelector(`audio[data-info='${padsKey}']`)
-//     if(!audio) return;
-    
-//     const nameAudio = audio.getAttribute('data-id')
-//     textDisplay.textContent = nameAudio
-
-//     audio.currentTime = 0
-//     audio.play()
-// }
-
-// // change theme & play audio
-// window.addEventListener("click", changeTheme2)
-// window.addEventListener('click', clickAudio)
-
-
-// // Function that display different theme colors and song
-// function changeTheme2(e) {
-//     if (e.code == "Enter" && themeContent == 'song 2') {
-//         themeContent = 'song 2'
-//         document.documentElement.setAttribute('data-theme', 'default');
-//         themeContent.textContent = 'song 2'
-//         window.removeEventListener('click', clickAudio2)
-//         window.addEventListener('click', clickAudio)
-        
-//     } else if (e.code == "Enter" && themeContent == 'song 1') {
-//         themeContent = 'song 1'
-//         document.documentElement.setAttribute('data-theme', 'song2');
-//         themeContent.textContent = 'song 1'
-//         window.removeEventListener('click', clickAudio)
-//         window.addEventListener('click', clickAudio2)
-//     }
-// }
-
-
+// window.addEventListener('click', function(e){
+//     const padsName = e.srcElement.className;
+//     const audio = document.querySelector(`audio[class='${padsName}']`)
+//     console.log(audio);
+// });
 
 
 //volume management
@@ -161,6 +114,6 @@ volum.forEach(element => {
 
 // note avant d'aller dodo:
 // A RAJOUTER :
-// ajouter le changement de mots + réparer les modes 1 2 ok mais pas 1 2 1
+// ajouter on click + barre de chargement ou animation début
 
 
